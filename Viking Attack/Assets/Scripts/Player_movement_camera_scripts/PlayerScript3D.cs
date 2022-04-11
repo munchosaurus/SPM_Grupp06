fa5cd8df2982a714a6e5561be863b46bbfcd26f9
munchosaurus <martinkings1@live.se>
2022-04-11 15:58:18 +0200

    Revert "Added comments to player/camera movement"
    This reverts commit dbb3d1457d2921a639742075212b0f099abd2fa3.

--- Viking Attack/Assets/Scripts/Player_movement_camera_scripts/PlayerScript3D.cs
+++ Viking Attack/Assets/Scripts/Player_movement_camera_scripts/PlayerScript3D.cs
@@ -4,27 +4,28 @@ using UnityEngine;
 
 public class PlayerScript3D : MonoBehaviour
 {
+    private MyRigidbody3D rb;
     [SerializeField] private State[] states;
-    private MyRigidbody3D myRigidbody;
-    private StateMachine stateMachine;
     public float jumpForce = 10f;
     public float acceleration  = 10f;
     public Camera mainCamera;
     public bool firstPerson;
-    
+    private StateMachine st;
     void Awake()
     {
-        myRigidbody = GetComponent<MyRigidbody3D>();
+        rb = GetComponent<MyRigidbody3D>();
         if(states.Length > 0)    
-            stateMachine = new StateMachine(this,states);
+            st = new StateMachine(this,states);
     }
 
     void Update()
     {  
-        //If there are any added states in the unity inspector
         if(states.Length > 0)      
-            stateMachine.Update();
+            st.Update();
     }
-    //Returns myRigidbody
-    public MyRigidbody3D MyRigidbody3D => myRigidbody;
+
+    public MyRigidbody3D getMyRigidbody3D()
+    {
+        return rb;
+    }
 }
