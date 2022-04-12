3362b8f57dd712c71f7c4d9eb1074084d13b732d
munchosaurus <martinkings1@live.se>
2022-04-12 21:06:37 +0200

    Big ass flush

--- Viking Attack/Assets/Scripts/Interactable_Logic_Scripts/Base/BaseObjectActivation.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-//Base for all activations
-public abstract class BaseObjectActivation : MonoBehaviour
-{
-    virtual public void activate(){}
-}
