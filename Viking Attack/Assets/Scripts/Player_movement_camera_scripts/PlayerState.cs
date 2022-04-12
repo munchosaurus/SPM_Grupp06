7fe83508b9ebb8b5fcf9c86b482e240a04c43300
GUST <gustavkjonsson01@gmail.com>
2022-04-12 11:37:58 +0200

    Player Movement Camera Script Removal
    Removes files that are causing compilation errors, so that I can enter scenes to test things. Feel free to overwrite this in case of a merge conflict. It is not meant to be a permanent change.

--- Viking Attack/Assets/Scripts/Player_movement_camera_scripts/PlayerState.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class PlayerState : State
-{
-    private PlayerScript3D p;
-    public PlayerScript3D player => p = p ?? (PlayerScript3D)owner;
-}
