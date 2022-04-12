8e9c5cd59ef5166cb55e7cb05b27434e57d283cd
munchosaurus <martinkings1@live.se>
2022-04-12 22:00:45 +0200

    Added test bridge and altered some code for bridge script

--- Viking Attack/Assets/Scripts/Interactable_Logic_Scripts/BridgeActivation.cs
+++ Viking Attack/Assets/Scripts/Interactable_Logic_Scripts/BridgeActivation.cs
@@ -4,27 +4,35 @@ using UnityEngine;
 
 public class BridgeActivation : BaseObjectActivation
 {
-    [SerializeField] private float roatitionSpeed;
+    
+    [SerializeField] private float rotationSpeed;
     //Sets to the starting rotation
-    private Quaternion tragetRotation = Quaternion.Euler(0,0,-90);
+    private Quaternion targetRotation;
     //A boolean if the bridge is down or upp
-    private bool bridgeDown = false;
+    private bool bridgeDown;
     public override void activate()
     {
         //Moves the bridge shaft by 90 degrees
         if(!bridgeDown)
         {
-            tragetRotation = Quaternion.Euler(0,0,0);
+            targetRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y,0);
             bridgeDown = true;
         }else
         {
-            tragetRotation = Quaternion.Euler(0,0,-90);
+            targetRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y,-90);
             bridgeDown = false;
         }      
     }
+
+    void Start()
+    {
+        targetRotation = transform.rotation;
+        
+    }
+
     void Update()
     {
         //Moves the bridge in a motion (Not teleporting)
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, tragetRotation, roatitionSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
