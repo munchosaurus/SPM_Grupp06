b9a0b9b32339f5282aa165d83148d4176c3d2683
munchosaurus <martinkings1@live.se>
2022-04-12 21:12:42 +0200

    Added project

---
+++ Viking Attack/Assets/Scripts/Character_scripts/GlobalPlayerInfo.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+// Who to blame: Martin Kings
+
+// Container for all player specifics, will add experience gained, HP, level, items owned etc...
+public class GlobalPlayerInfo : MonoBehaviour
+{
+    public static string name;
+    public static Color skinColor;
+
+    // Gets called upon during game launch, the main menu sets the player name
+    public static void SetPlayerName(string insertedName)
+    {
+        name = insertedName;
+    }
+
+    // Gets called upon during game launch, the main menu sets the player skin color
+    public static void SetSkinColor(Color insertedColor)
+    {
+        skinColor = insertedColor;
+    }
+
+    // Returns the player name
+    public string GetName()
+    {
+        return name;
+    }
+
+    // Returns the player skin color
+    public Color GetSkinColor()
+    {
+        return skinColor;
+    }
+    
+}
---
+++ Viking Attack/Assets/Scripts/Player_movement_camera_scripts/PlayerRunState.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PlayerState/RunState")]
+//Used as a state when the player inputs for Horizontal and Vertical movement
+public class PlayerRunState : PlayerState
+{
+    private Vector3 input;
+    public override void Enter()
+    {
+        
+    }
+    public override void Update()
+    {
+        //get player input
+        input = Vector3.right * Input.GetAxisRaw("Horizontal") + Vector3.forward * Input.GetAxisRaw("Vertical");
+        input = Player.mainCamera.transform.rotation * input;
+        input = Vector3.ProjectOnPlane(input,Player.MyRigidbody3D.Grounded().normal);
+        input = input.normalized * Player.acceleration;
+
+        //If player is grounded set input vector to follow the ground 
+        if(!Player.MyRigidbody3D.GroundedBool())
+            input = new Vector3(input.x,0f,input.z);
+        Player.MyRigidbody3D.velocity += input * Player.acceleration;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+            stateMachine.ChangeState<PlayerJumpState>();
+        else if(Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
+            stateMachine.ChangeState<PlayerBaseState>();
+    }
+    public override void Exit()
+    {
+        
+    }
+}
