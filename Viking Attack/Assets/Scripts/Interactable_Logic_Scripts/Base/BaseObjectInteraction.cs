9a16f5b8c9e1c9c48a1c9caa4ecbfaa462a25d14
munchosaurus <martinkings1@live.se>
2022-04-12 23:55:40 +0200

    Deleted bad version

--- Viking Attack/Assets/Scripts/Interactable_Logic_Scripts/Base/BaseObjectInteraction.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-//Base for all interactions
-public abstract class BaseObjectInteraction : MonoBehaviour
-{
-    virtual public void InteractedWith(){}
-}
