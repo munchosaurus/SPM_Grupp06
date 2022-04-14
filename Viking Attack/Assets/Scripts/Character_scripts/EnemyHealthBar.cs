4ab67c72ad76c5e47867a7f546e2f4e55193e4d9
munchosaurus <martinkings1@live.se>
2022-04-14 10:49:17 +0200

    Fetched martin_test

--- Viking Attack/Assets/Scripts/Character_scripts/EnemyHealthBar.cs
+++ Viking Attack/Assets/Scripts/Character_scripts/EnemyHealthBar.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 public class EnemyHealthBar : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    
+    
     public void Display()
-    {
+    {   
+        
         var wantedPos = Camera.main.WorldToScreenPoint (target.position);
         gameObject.transform.position = wantedPos;
+    }
+
+    public void Activate()
+    {
+        if (!gameObject.activeSelf)
+        {
+            gameObject.SetActive(true);
+        }
+    }
+    
+    public void Deactivate()
+    {
+        if (gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
 
--- Viking Attack/Assets/Scripts/Character_scripts/PlayerActivateEnemyHealthBar.cs
+++ Viking Attack/Assets/Scripts/Character_scripts/PlayerActivateEnemyHealthBar.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace DefaultNamespace
@@ -5,21 +6,37 @@ namespace DefaultNamespace
     public class PlayerActivateEnemyHealthBar : MonoBehaviour
     {
         private GameObject targetHealthBar;
+        private bool hitting = false;
+        private GameObject hitObject;
 
         void Update()
         {
-            //Sends a raycast to check for colliders in the Enemy layer
-            RaycastHit hit;
-
-            if (Physics.SphereCast(gameObject.transform.Find("Main Camera").transform.position, 2f,
-                    gameObject.transform.Find("Main Camera").transform.forward, out hit, 25, LayerMask.GetMask("Enemy")))
-            {
-                targetHealthBar = hit.collider.gameObject;
-                targetHealthBar.transform.Find("Parent").gameObject.SetActive(true);
-                GameObject child = targetHealthBar.transform.Find("Parent").gameObject.transform.Find("Health_bar").gameObject;
-                child.GetComponent<EnemyHealthBar>().Display();
-            }
-            
+            //     //Sends a raycast to check for colliders in the Enemy layer
+            //     RaycastHit hit;
+            //
+            //     if (Physics.SphereCast(gameObject.transform.Find("Main Camera").transform.position, 2f,
+            //             gameObject.transform.Find("Main Camera").transform.forward, out hit, 25)
+            //     {
+            //         GameObject go = hit.transform.gameObject ;
+            //         
+            //         if (hitObject == null)
+            //         {
+            //             try
+            //             {
+            //                 go.transform.Find("Parent").gameObject.transform.Find("Health_bar").SendMessage("Activate");
+            //             }
+            //             catch (Exception e)
+            //             {
+            //                 Debug.Log("Funkar ej bra");
+            //             }
+            //             
+            //         }
+            //         else if( hitObject.GetInstanceID() == go.GetInstanceID() )
+            //         {
+            //             hitObject.SendMessage( "Display" );
+            //         }
+            //     }
+            // }
         }
     }
 }
